c520a0f9195308623d44dd7dc976d471f11b85d4
지호 박 <aquashdw@mindslab.ai>
2020-01-15 18:00:32 +0900

    flask initial commit

---
+++ flask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/assets/assets/images/<image_name>')
+def get_image_assets(image_name):
+    return redirect('/static/assets/assets/images/' + image_name)
+
+
+@app.route('/assets/<manifest>')
+def get_manifest(manifest):
+    return redirect('/static/assets/' + manifest)
+
+
+@app.route('/assets/fonts/<font_file_name>')
+def get_fonts(font_file_name):
+    return redirect('/static/assets/fonts/' + font_file_name)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
